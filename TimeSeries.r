5cb235aa1ff5aedbbc6d060d5390fb0f7df527d6
Anant Agarwal <aagarwal_be16@thapar.edu>
2019-02-08 02:09:33 -0800

    Function made for deseasonlizing the data

--- TimeSeries.r
+++ TimeSeries.r
@@ -1,4 +1,6 @@
 
+
+
 library("rio")
 library("ggplot2")
 library("forecast")
@@ -6,6 +8,8 @@ library("tseries")
 library("tidyverse")
 library(readxl)
 library(zoo)
+
+
 
 data <- read.csv("Monthly_data_cmo.csv")
 data <- data.frame(data)
@@ -19,52 +23,26 @@ data$min_price = tsclean(ts(data[, c('min_price')]))
 data$max_price = tsclean(ts(data[, c('max_price')]))
 data$modal_price = tsclean(ts(data[, c('modal_price')]))
 
-data1 <- subset(data, APMC =="Ahmednagar")
-# data2015 <- subset(data, Year ==2015)
-# data2016 <- subset(data, Year ==2016)
+season <- function(source,apmc, commodity,attribute,plot = FALSE,acf_test = FALSE ){
+    data1 <- subset(source, APMC == apmc)
+    data1 <- subset(data1, Commodity == commodity)
+    data1$arrivals_in_qtl = tsclean(ts(data1[, c('arrivals_in_qtl')]))
+    data1$min_price = tsclean(ts(data1[, c('min_price')]))
+    data1$max_price = tsclean(ts(data1[, c('max_price')]))
+    data1$modal_price = tsclean(ts(data1[, c('modal_price')]))
 
-data1 <- subset(data1, Commodity =="Bajri")
+    count_ma = ts(na.omit(data1[[attribute]]),frequency = 3)
+    decomp = stl(count_ma,"periodic")
+    
+    deseasonal_cnt <- seasadj(decomp)
+    
+    if(plot ==  TRUE){
+        plot(decomp) 
+    }
+    if(acf_test == TRUE){
+        print(adf.test(count_ma,alternative = "stationary"))
+    }
+    count_dl = diff(deseasonal_cnt,differences = 1)
+}
 
-data1$arrivals_in_qtl = tsclean(ts(data1[, c('arrivals_in_qtl')]))
-
-data1$min_price = tsclean(ts(data1[, c('min_price')]))
-
-data1$max_price = tsclean(ts(data1[, c('max_price')]))
-data1$modal_price = tsclean(ts(data1[, c('modal_price')]))
-
-head(data1)
-
-str(data1)
-
-ggplot() +
-geom_line(data= data1 , aes(y = arrivals_in_qtl , x= as.numeric(date)))
-
-ggplot() +
-geom_line(data= data1 , aes(y = min_price , x= as.numeric(date)))
-
-ggplot() +
-geom_line(data= data1 , aes(y = max_price , as.numeric(date)))
-
-ggplot() +
-geom_line(data= data1 , aes(y = modal_price , as.numeric(date)))
-
-count_ma = ts(na.omit(data1$arrivals_in_qtl),frequency = 3)
-decomp = stl(count_ma,"periodic")
-
-
-deseasonal_cnt <- seasadj(decomp)
-plot(decomp)
-
-adf.test(count_ma,alternative = "stationary")
-
-Acf(count_ma, main='')
-
-Pacf(count_ma, main='')
-
-count_dl = diff(deseasonal_cnt,differences = 1)
-plot(count_dl)
-
-adf.test(count_dl,alternative = "stationary")
-
-Acf(count_dl, main='')
-Pacf(count_dl, main='')
+ a <- season(source = data, apmc = "Ahmednagar",commodity = "Bajri",attribute = "arrivals_in_qtl",plot = TRUE,acf_test = TRUE)
